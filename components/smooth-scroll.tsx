f9bcf81c1f91f1941a2baa6ebb9219a34edf0abf
JhairAlexby <jhair.alexby1@gmail.com>
2025-10-25 18:21:34 -0600

    1.2

---
+++ components/smooth-scroll.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { useEffect } from 'react'
+import Lenis from 'lenis'
+
+export default function SmoothScroll() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.1,
+      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+      // Nota: algunas versiones de Lenis no exponen 'smoothTouch'.
+      // Si quieres ajustar el comportamiento táctil, prueba 'touchMultiplier' o valores de 'duration'.
+    })
+
+    let rafId: number
+    const raf = (time: number) => {
+      lenis.raf(time)
+      rafId = requestAnimationFrame(raf)
+    }
+
+    rafId = requestAnimationFrame(raf)
+
+    return () => {
+      cancelAnimationFrame(rafId)
+      lenis.destroy()
+    }
+  }, [])
+
+  return null
+}
